6969910fdd7a6365a3f2b01c9ea9314279abcf51
Bepis1559 <axl2001@abv.bg>
2023-10-19 20:56:08 +0300

    forgot all changes, but that should be it

---
+++ Stock.API/Interfaces/IBaseStock.cs
@@ -0,0 +1,11 @@
+﻿using Stocks.Enums;
+
+namespace Stocks.Interfaces
+{
+    public interface IBaseStock
+    {
+        public TimeSeries TimeSeries { get; set; }
+        public string? Symbol { get; set; }
+        public Interval? Interval { get; set; }
+    }
+}
--- Stock.API/Program.cs
+++ Stock.API/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.OpenApi.Models;
+using Stocks.services;
+using Stocks.utils;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<URL_Maker>();
+builder.Services.AddScoped<AlphaVantageService>();
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
+    });
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Stocks", Version = "v1" });
+    c.EnableAnnotations();
+});
 
 var app = builder.Build();
 

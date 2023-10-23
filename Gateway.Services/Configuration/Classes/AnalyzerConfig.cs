0ca2380c92d20c1c109410d125bef6c80a603a2f
Bozhidar <mrarthur0507@gmail.com>
2023-10-23 18:32:39 +0300

    Gateway services fix

---
+++ Gateway.Services/Configuration/Classes/AnalyzerConfig.cs
@@ -0,0 +1,19 @@
+﻿using Gateway.Services.Configuration.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gateway.Services.Configuration.Classes
+{
+    public class AnalyzerConfig : BaseConfig, IAnalyzerConfig
+    {
+        public string CurrentProfit { get; set; }
+        public string DailyReturnProfit { get; set; }
+        public string PercentageChange { get; set; }
+        public string PortfolioRisk { get; set; }
+
+
+    }
+}
---
+++ Gateway.Services/Configuration/Interfaces/IAnalyzerConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gateway.Services.Configuration.Interfaces
+{
+    public interface IAnalyzerConfig : IBaseConfig
+    {
+        public string CurrentProfit { get; set; }
+        public string DailyReturnProfit { get; set; }
+        public string PercentageChange { get; set; }
+
+        public string PortfolioRisk { get; set; }
+
+    }
+}

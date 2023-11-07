a1155e8a52cd5b5c20b680e9f569259de3b646e2
EmilStanchev <emilstan4ev03@gmail.com>
2023-11-07 13:41:15 +0200

    deleted cached

---
+++ Accounts.API.Services/Implementation/AuthorizationService.cs
@@ -0,0 +1,112 @@
+﻿using Accounts.API.Services.Interfaces;
+using System.Text;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+//using Microsoft.AspNetCore.Http;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace Accounts.API.Services.Implementation
+{
+    public class AuthorizationService : IAuthorizationService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IConfiguration _config;
+        private readonly string _secretKey;
+        public AuthorizationService(IHttpContextAccessor httpContextAccessor,IConfiguration config)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _config = config;
+            _secretKey=_config.GetSection("AppSettings")["SecretKey"];
+        }
+        public string GenerateJwtToken(string username)
+        { var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secretKey));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.Name, username),
+            };
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(1),
+                signingCredentials: signingCredentials
+            );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+            return tokenString;
+        }
+
+        public string GetTokenFromSessionStorage(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveTokenInSessionStorage(string token)
+        {
+            var response = _httpContextAccessor.HttpContext.Response;
+            var cookieName = "token";
+            response.Cookies.Append(cookieName, token, new CookieOptions
+            {
+                Expires = DateTime.Now.AddMinutes(10),
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.None
+            });
+        }
+        public bool CheckToken(string token)
+        {
+            return CheckToken(token, out string userId);
+        }
+        private bool CheckToken(string token, out string userId)
+        {
+            userId = null;
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var validationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secretKey)),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+
+            try
+            {
+                tokenHandler.ValidateToken(token, validationParameters, out SecurityToken validatedToken);
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                userId = jwtToken.Claims.First(x => x.Type == ClaimTypes.Name).Value;
+                if (jwtToken.ValidTo>DateTime.Now.ToUniversalTime())
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private string GenerateRandomSecretKey(int length)
+        {
+            const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_=+";
+            StringBuilder result = new StringBuilder();
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] uintBuffer = new byte[sizeof(uint)];
+
+                while (length-- > 0)
+                {
+                    rng.GetBytes(uintBuffer);
+                    uint num = BitConverter.ToUInt32(uintBuffer, 0);
+                    result.Append(validChars[(int)(num % (uint)validChars.Length)]);
+                }
+            }
+            return result.ToString();
+        }
+    }
+}

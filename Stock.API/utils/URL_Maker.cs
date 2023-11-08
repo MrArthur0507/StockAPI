62bdb6e7692452ad6afdcf2ac64153c29f00a0b2
Bepis1559 <axl2001@abv.bg>
2023-11-08 16:33:39 +0200

    .

---
+++ Stock.API/utils/URL_Maker.cs
@@ -0,0 +1,25 @@
+﻿using Stocks.Interfaces;
+using Stocks.Models;
+
+namespace Stocks.utils
+{
+    public class URL_Maker
+    {
+        private readonly string API_KEY = "VV5H4JTPBS2O605U";
+
+        private readonly string host = "https://www.alphavantage.co/";
+       
+
+      public string GetURL_WithBaseStock(IBaseStock stock)
+        {
+            if(stock == null)
+            {
+                throw new ArgumentNullException("No stock provided , got : " + stock);
+            }
+            string urlToCall = $"{host}query?function=TIME_SERIES_{stock.TimeSeries}&symbol={stock.Symbol}&interval={(int?)stock.Interval}min&apikey={API_KEY}";
+            return urlToCall;
+        }
+
+
+    }
+}

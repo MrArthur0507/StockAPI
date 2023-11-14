369a06fe05cb71024af050b1044bbb2fe40afe4d
Bepis1559 <axl2001@abv.bg>
2023-11-14 19:49:33 +0200

    Added full implementation of sqlite .

--- Stock.API/Controllers/StockController.cs
+++ Stock.API/Controllers/StockController.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿//using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Stocks.Interfaces;
+//using Microsoft.Extensions.Logging;
+//using Newtonsoft.Json;
+//using Newtonsoft.Json.Linq;
 using Stocks.Models;
 using Stocks.services;
+using Stocks.utils;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Stocks.Controllers
 {
@@ -10,24 +16,55 @@ namespace Stocks.Controllers
     public class StockController : ControllerBase
     {
         private readonly AlphaVantageService _alphaVantageService;
+        private readonly DbService _dbService;
+        private readonly ILogger<StockController> _logger;
 
-        public StockController(AlphaVantageService alphaVantageService)
+        public StockController(AlphaVantageService alphaVantageService, DbService dbService, ILogger<StockController> logger)
+        //
+
         {
             _alphaVantageService = alphaVantageService;
+            _dbService = dbService;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetStockData([FromQuery] Stock stock)
         {
-            try
+            // first pass the stock to the IsStockIsPresentInDb method
+            // there we check if there is such a stock in the db
+            // if so , get the data from there , and if not
+            // proceed with the API call and add the data to the db
+
+
+
+
+            var stockDataFromDb = await _dbService.GetDataFromDb(stock);
+
+            if (stockDataFromDb == null)
             {
-                var data = await _alphaVantageService.GetStockData(stock);
-                return Ok(data);
+                try
+                {
+                    var data = await _alphaVantageService.GetStockData(stock);
+                    await _dbService.AddToDb(data);
+                    return Ok(data);
+                }
+                catch (HttpRequestException ex)
+                {
+                    return BadRequest($"Error calling AlphaVantage API: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest($"An unexpected error occurred: {ex.Message}");
+                }
             }
-            catch (Exception ex)
+            else
             {
-                return BadRequest($"Error: {ex.Message}");
+                _logger.LogCritical("This data is from the DB ");
+                return Ok(stockDataFromDb);
             }
+
+
         }
     }
 
